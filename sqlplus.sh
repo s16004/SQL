3d28beaabd8123052908adad59199cf83e1c967f
s16004 <s16004@std.it-college.ac.jp>
2017-10-05 17:50:16 +0900

    first commit

--- sqlplus.sh
+++ sqlplus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-USER=s16004
-PASS=password
+USER=hr
+PASS=hr
 ADDR=172.16.40.4
 PORT=1521
 SID=orcl
@@ -11,6 +11,5 @@ export LD_LIBRARY_PATH=${BASE}:${LD_LIBRARY_PATH}
 export SQLPATH=${BASE}:${SQL_PATH}
 export no_proxy="127.0.0.1,localhost,$ADDR"
 export NO_PROXY="$no_proxy"
-NLS_LANG=JAPANESE_JAPAN.AL32UTF8
-export NLS_LANG
+
 rlwrap $BASE/sqlplus $USER/$PASS@$ADDR:$PORT/$SID
