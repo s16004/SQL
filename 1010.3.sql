3f845e3acb8ef8196a12abebf7536bf3d3290495
s16004 <s16004@std.it-college.ac.jp>
2017-10-11 14:01:59 +0900

    commit

--- 1010.3.sql
+++ 1010.3.sql
@@ -1,4 +1,4 @@
-select employee_id, last_name, salary, round(salary * 0.155) "New Salary"
+select employee_id, last_name, salary, round(salary * 0.155 + salary) "New Salary"
 from employees
 order by salary
 fetch first 5 rows only;
