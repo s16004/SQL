4c5ed13f53d193206c786134c625e099166aafb4
s16004 <s16004@std.it-college.ac.jp>
2017-10-17 17:22:53 +0900

    commit

---
+++ 1017.3.sql
@@ -0,0 +1,6 @@
+select last_name,job_title,department_id,department_name
+from employees e 
+	join departments d using(department_id)
+	join jobs j using(job_id)
+	join locations l using(location_id)
+where city = 'Toronto';
---
+++ 1017.5.sql
@@ -0,0 +1,6 @@
+select w.last_name "Employee", w.employee_id "EMP#",m.last_name"Manager", m.manager_id"Mgr#"
+from employees w left outer join employees m
+on w.manager_id = m.employee_id;
+
+
+
